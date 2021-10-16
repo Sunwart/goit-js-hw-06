c28f19baad584a750fb7928917bf798e21fea953
Sunwart <li.sa.vectorart@gmail.com>
2021-10-16 04:07:55 +0300

    Task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputElem = document.querySelector('input');
+const createBtnElem = document.querySelector('button[data-create]');
+const destroyBtnElem = document.querySelector('button[data-destroy]');
+const boxesElem = document.querySelector('#boxes');
+
+let amount = 0;
+
+inputElem.addEventListener('input', event => {
+  amount = event.currentTarget.value;
+});
+
+createBtnElem.addEventListener('click', event => {
+  createBoxes(amount);
+});
+
+function createBoxes(amount) {
+  let html = '';
+  for (let i = 0; i < amount; i += 1) {
+    const color = getRandomHexColor();
+    html += `<div style="width: ${i * 10 + 30}px; height: ${
+      i * 10 + 30
+    }px; background-color: ${color}"></div>`;
+  }
+  console.log(html);
+  boxesElem.insertAdjacentHTML('afterbegin', html);
+}
+
+destroyBtnElem.addEventListener('click', event => {
+  boxesElem.innerHTML = '';
+});
