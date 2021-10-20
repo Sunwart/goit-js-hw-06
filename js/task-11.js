cf3fe7b528d3ca1316e59802e503694d30159a65
Sunwart <li.sa.vectorart@gmail.com>
2021-10-20 21:58:16 +0300

    JS practice

--- js/task-11.js
+++ js/task-11.js
@@ -1,43 +1,30 @@
-const colorPalette = document.querySelector('.color-palette');
-const output = document.querySelector('.output');
+const lodashText = document.querySelector('.lodash-test');
+lodashText.textContent = _.add(128, 13);
 
-colorPalette.addEventListener('click', selectColor);
+document.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    console.log('Scroll handler call every 5000ms');
+  }, 5000),
+);
 
-// This is where delegation «magic» happens
-function selectColor(event) {
-  if (event.target.nodeName !== 'BUTTON') {
-    return;
-  }
+document.addEventListener(
+  'scroll',
+  _.debounce(() => {
+    console.log('Scroll handler call after 3000ms pause');
+  }, 3000),
+);
 
-  const selectedColor = event.target.dataset.color;
-  output.textContent = `Selected color: ${selectedColor}`;
-  output.style.color = selectedColor;
-}
-
-// Some helper functions to render palette items
-createPaletteItems();
-
-function createPaletteItems() {
-  const items = [];
-  for (let i = 0; i < 120; i++) {
-    const color = getRangomColor();
-    const item = document.createElement('button');
-    item.type = 'button';
-    item.dataset.color = color;
-    item.style.backgroundColor = color;
-    item.classList.add('item');
-    items.push(item);
-  }
-
-  colorPalette.append(...items);
-}
-
-function getRangomColor() {
-  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-}
-
-function getRandomHex() {
-  return Math.round(Math.random() * 256)
-    .toString(16)
-    .padStart(2, '0');
-}
+document.addEventListener(
+  'scroll',
+  _.debounce(
+    () => {
+      console.log('Scroll handler call on every event stream start');
+    },
+    4000,
+    {
+      leading: true,
+      trailing: false,
+    },
+  ),
+);
